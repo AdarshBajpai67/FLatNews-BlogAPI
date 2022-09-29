4fb51e9643f5548bf68d0887e3f8f49458732a87
Tolu Joel <toluisjoel@gmail.com>
2022-09-29 11:02:39 +0100

    post list view

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+    list_display = ('title','status', 'author',)
+    list_filter =  ('published_date',)
+    prepopulated_fields = {'slug':('title',)}
+
+admin.site.register(Post, PostAdmin)
