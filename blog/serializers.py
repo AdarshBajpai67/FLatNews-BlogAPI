51490867a38fa710720e70bfe52053d1592ebd56
Tolu Joel <toluisjoel@gmail.com>
2022-09-29 10:53:37 +0100

    Post serializer

---
+++ blog/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from .models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model  = Post
+        fields = (
+            'author',
+            'title',
+            'body',
+            'thumbnail',
+            'status',
+            'created_at',
+        )
