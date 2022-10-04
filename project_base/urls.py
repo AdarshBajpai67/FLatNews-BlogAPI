fae00e071c312bb38502c83fb92217c41f7645d5
Tolu Joel <toluisjoel@gmail.com>
2022-10-04 11:21:56 +0100

    added documentation

--- project_base/urls.py
+++ project_base/urls.py
@@ -17,15 +17,21 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from drf_spectacular.views import SpectacularAPIView
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('blog.urls')),
+    
+    # authentication and authorization
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api/v1/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    
+    # schema and documentation
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/docs/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
 
 if settings.DEBUG:
